38d9a7a6f5b9d2a16b79df5251e433d0a9ef3529
switchit1 <@switchit1>
2022-12-26 19:41:51 +0100

    feat: check whether login is needed

--- background.js
+++ background.js
@@ -1,8 +1,1 @@
 "use strict";
-
-// On Chrome Install
-chrome.runtime.onInstalled.addListener(function (details) {
-    if (details.reason === "install") {
-        chrome.tabs.create({ url: "https://chat.openai.com/auth/login" });
-    }
-});
